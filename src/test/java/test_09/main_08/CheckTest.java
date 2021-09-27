6eb9d33a186cd743127945b09f73cd6bb516b1ad
hmoon826 <hmoon826@gmail.com>
2021-09-27 11:25:42 +0900

    Edge, Check TEST 커밋

--- src/test/java/test_09/main_08/CheckTest.java
+++ src/test/java/test_09/main_08/CheckTest.java
@@ -1,9 +1,24 @@
+package test_09.main_08;
+
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
 
 public class CheckTest {
 
     @Test
-    void 체크검증() {
-        
+    void 체크_제로() {
+        Check check = new Check(3);
+        assertThat(check.checkZero(0)).isTrue();
+        assertThat(check.checkZero(1)).isTrue();
+        assertThat(check.checkZero(2)).isTrue();
+    }
+
+    @Test
+    void 체크_1_변경() {
+        Check check = new Check(3);
+        check.setCheck(0);
+        assertThat(check.checkZero(0)).isFalse();
     }
 }
--- src/test/java/test_09/main_08/EdgeTest.java
+++ src/test/java/test_09/main_08/EdgeTest.java
@@ -1,2 +1,27 @@
-package test_09.main_08;public class EdgeTest {
+package test_09.main_08;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+import java.util.PriorityQueue;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class EdgeTest {
+
+    @Test
+    void 큐_edge객체_저장() {
+        Main08 main08 = new Main08();
+        List<List<Edge>> graph = main08.getGraph(5);
+        Check ch = new Check(6);
+        Edge edge = new Edge(1, 0);
+        PriorityQueue<Edge> pQue = new PriorityQueue<>();
+        pQue.offer(edge);
+
+        assertThat(edge.costCal(pQue, graph, ch)).isEqualTo(0);
+        assertThat(ch.checkZero(0)).isTrue();
+        assertThat(pQue.poll()).isEqualTo(new Edge(1, 0));
+    }
+
 }
