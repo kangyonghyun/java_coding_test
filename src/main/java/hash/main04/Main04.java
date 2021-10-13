35a73f1ea8c20ed78fa4e1fcc7f0ac649b7e0cdd
hmoon826 <hmoon826@gmail.com>
2021-10-13 10:28:04 +0900

    hash - 모든 아나그램 찾기(hash & sliding window) 리펙터링

--- src/main/java/hash/main04/Main04.java
+++ src/main/java/hash/main04/Main04.java
@@ -15,27 +15,43 @@ public class Main04 {
         System.out.println(main04.solution(s, t));
     }
 
-    public int solution(String s, String t) {
-        int answer = 0;
+    public int solution(String source, String target) {
+        //sMap 초기화
+        Map<Character, Integer> sMap = sourceMapSetting(source, target);
+        //tMap 초기화
+        Map<Character, Integer> tMap = targetMapSetting(target);
+        // 아나그램이 되는 횟수 계산
+        return countAnagram(sMap, tMap, source, target);
+    }
 
-        Map<Character, Integer> map = new HashMap<>();
-        char[] arr = s.toCharArray();
-        for (int i = 0; i < t.length() - 1; i++) {
-            map.put(arr[i], map.getOrDefault(arr[i], 0) + 1);
-        }
-        Map<Character, Integer> tMap = new HashMap<>();
-        for (char c : t.toCharArray()) {
-            tMap.put(c, tMap.getOrDefault(c, 0) + 1);
-        }
-
+    private int countAnagram(Map<Character, Integer> sMap, Map<Character, Integer> tMap, String source, String target) {
+        int count = 0;
         int lt = 0;
-        for (int rt = t.length() - 1; rt < s.length(); rt++) {
-            map.put(arr[rt], map.getOrDefault(arr[rt], 0) + 1);
-            if (map.equals(tMap)) answer++;
-            map.put(arr[lt], map.get(arr[lt]) - 1);
-            if (map.get(arr[lt]) == 0) map.remove(arr[lt]);
+        char[] arr = source.toCharArray();
+        for (int rt = target.length() - 1; rt < source.length(); rt++) {
+            sMap.put(arr[rt], sMap.getOrDefault(arr[rt], 0) + 1);
+            if (sMap.equals(tMap)) count++;
+            sMap.put(arr[lt], sMap.get(arr[lt]) - 1);
+            if (sMap.get(arr[lt]) == 0) sMap.remove(arr[lt]);
             lt++;
         }
-        return answer;
+        return count;
+    }
+
+    private Map<Character, Integer> sourceMapSetting(String source, String target) {
+        Map<Character, Integer> map = new HashMap<>();
+        char[] arr = source.toCharArray();
+        for (int i = 0; i < target.length() - 1; i++) {
+            map.put(arr[i], map.getOrDefault(arr[i], 0) + 1);
+        }
+        return map;
+    }
+
+    private Map<Character, Integer> targetMapSetting(String target) {
+        Map<Character, Integer> map = new HashMap<>();
+        for (char c : target.toCharArray()) {
+            map.put(c, map.getOrDefault(c, 0) + 1);
+        }
+        return map;
     }
 }
