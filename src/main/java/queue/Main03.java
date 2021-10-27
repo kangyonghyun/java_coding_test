e70b2b9b71f8a44096ed4c852e6c9bf858183c37
hmoon826 <hmoon826@gmail.com>
2021-10-27 17:51:03 +0900

    Queue - 응급실

---
+++ src/main/java/queue/Main03.java
@@ -0,0 +1,52 @@
+package queue;
+
+import java.util.*;
+
+public class Main03 {
+
+    static class Person {
+        int id;
+        int priority;
+
+        Person(int id, int priority) {
+            this.id = id;
+            this.priority = priority;
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+        Main03 main03 = new Main03();
+        System.out.println(main03.solution(n, m , arr));
+    }
+
+    private int solution(int n, int m, int[] arr) {
+        int answer = 0;
+        Queue<Person> queue = new LinkedList<>();
+        for (int i = 0; i < n; i++) {
+            queue.offer(new Person(i, arr[i]));
+        }
+
+        while (!queue.isEmpty()) {
+            Person tmp = queue.poll();
+            for (Person p : queue) {
+                if (p.priority > tmp.priority) {
+                    queue.offer(tmp);
+                    tmp = null;
+                    break;
+                }
+            }
+            if (tmp != null) {
+                answer++;
+                if (tmp.id == m) return answer;
+            }
+        }
+        return answer;
+    }
+}
